b2ad11fe90213ae8226dcd8947a42ca1b7bb4e24
Im-Rises <quentin-morel88@hotmail.com>
2023-04-10 17:46:03 -0400

    Added new controls

--- src/components/VideoAsciiPanel.tsx
+++ src/components/VideoAsciiPanel.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useRef, useState} from 'react';
+import React, {useRef, useState} from 'react';
 import VideoAscii from 'video-stream-ascii';
 
 export const VideoAsciiPanel: React.FC = () => {
@@ -6,6 +6,19 @@ export const VideoAsciiPanel: React.FC = () => {
 	const [isVideoReady, setIsVideoReady] = useState<boolean>(false);
 	const [videoUrl, setVideoUrl] = useState<string | undefined>(undefined);
 	const videoRef = useRef<HTMLVideoElement>(null);
+	let frameRate = 30;
+	let skipAheadBehindInterval = 5;
+	// let videoCursor = 0;
+	const [currentTime, setCurrentTime] = useState(0);
+
+	const handleVideoCursorChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		const value = parseInt(e.target.value, 10);
+		// videoCursor = value;
+		setCurrentTime(value);
+		if (videoRef.current) {
+			videoRef.current.currentTime = value;
+		}
+	};
 
 	const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
 		const file = e.target.files?.[0];
@@ -16,6 +29,13 @@ export const VideoAsciiPanel: React.FC = () => {
 		setVideoUrl(URL.createObjectURL(file));
 	};
 
+	const moveVideoCursor = (offset: number) => {
+		if (videoRef.current) {
+			videoRef.current.currentTime += offset;
+			setCurrentTime(videoRef.current.currentTime + offset);
+		}
+	};
+
 	const togglePausePlay = async () => {
 		if (videoRef.current?.paused) {
 			await videoRef.current?.play();
@@ -23,32 +43,6 @@ export const VideoAsciiPanel: React.FC = () => {
 			videoRef.current?.pause();
 		}
 	};
-
-	// useEffect(() => {
-	// 	if (videoRef.current) {
-	// 		videoRef.current.addEventListener('canplay', () => {
-	// 			setIsVideoReady(true);
-	// 			console.log('video is ready');
-	// 		});
-	// 	}
-	//
-	// 	console.log('videoRef.current', videoRef.current);
-	// }, []);
-
-	// const handlePlayClick = () => {
-	// 	videoRef.current?.play().catch(e => {
-	// 		console.log(e);
-	// 	},
-	// 	);
-	// };
-	//
-	// const handlePauseClick = () => {
-	// 	videoRef.current?.pause();
-	// };
-
-	// const handleRewindClick = () => {
-	// 	videoRef.current.currentTime = 0;
-	// };
 
 	return (
 		<div>
@@ -63,19 +57,19 @@ export const VideoAsciiPanel: React.FC = () => {
 						}
 					}
 					onPlay={() => {
-						console.log('onPlay');
+						console.log('Hide resume button');
 					}}
 					onPause={() => {
-						console.log('onPause');
+						console.log('Show resume button');
 					}}
 					onEnded={() => {
-						console.log('onEnded');
+						setCurrentTime(0);
 					}}
+					onChangeCapture={() => {
+						console.log('New video cursor', videoRef.current?.currentTime);
+					}
+					}
 					/>
-					<button onClick={togglePausePlay}>Play/Pause</button>
-					{/* <button onClick={handlePlayClick}>Play</button> */}
-					{/* <button onClick={handlePauseClick}>Pause</button> */}
-					{/* <button onClick={handleRewindClick}>Rewind</button> */}
 				</div>
 			)}
 			{
@@ -85,6 +79,42 @@ export const VideoAsciiPanel: React.FC = () => {
 							charsPerLine={100}
 							charsPerColumn={100} frameRate={30} fontColor={'white'} backgroundColor={'black'}/>)
 					}
+					<button onClick={togglePausePlay}>Play/Pause</button>
+					<input type='range' value={currentTime} onChange={handleVideoCursorChange} min={0}
+						max={videoRef.current?.duration}/>
+					<button onClick={() => {
+						moveVideoCursor(+1 / frameRate);
+					}}>Next frame
+					</button>
+					<button onClick={() => {
+						moveVideoCursor(-1 / frameRate);
+					}}>Previous frame
+					</button>
+					{/* Button to skip to 5 seconds ahead */}
+					<button onClick={() => {
+						moveVideoCursor(skipAheadBehindInterval);
+					}
+					}>Skip ahead
+					</button>
+					<button onClick={() => {
+						moveVideoCursor(-skipAheadBehindInterval);
+					}}>Skip behind
+					</button>
+					{/* Selector to change the frame rate */}
+					<select onChange={e => {
+						const value = parseInt(e.target.value, 10);
+						frameRate = value;
+					}}
+					value={frameRate}
+					/>
+					{/* Selector to change the video speed */}
+					<select onChange={e => {
+						videoRef.current!.playbackRate = parseFloat(e.target.value);
+					}}/>
+					{/* Selector to change the ahead/behind interval */}
+					<select onChange={e => {
+						skipAheadBehindInterval = parseInt(e.target.value, 10);
+					}}/>
 				</div>
 			}
 		</div>
